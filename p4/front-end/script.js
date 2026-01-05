d2316b5570adcfa94b093fe9e8b9711f68864569
eduardo leite moreira <elmoreira.ce@gmail.com>
2026-01-05 15:38:21 -0300

    primero commit

---
+++ p4/front-end/script.js
@@ -0,0 +1,12 @@
+function primero() {
+    alert("Em breve o primeiro vídeo estará disponível!");
+    document.getElementById("video").src = "https://www.youtube.com/embed/U5zR6KPTAOU";
+}
+function segundo() {
+    alert("Em breve o segundo vídeo estará disponível!");
+    document.getElementById("video").src = "https://www.youtube.com/embed/08wTGkXf9uY";
+}
+function terceiro() {
+    alert("Em breve o terceiro vídeo estará disponível!");
+    document.getElementById("video").src = "https://www.youtube.com/embed/Uf4cw_NX7PI";
+}
